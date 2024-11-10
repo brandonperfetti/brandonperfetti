2676f34e2387f8f4e8145bf02df8b0fa555f898f
Brandon Perfetti <brandon@brandonperfetti.com>
2024-11-10 10:49:02 -0800

    Temp comment out code related to quote api.

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ require("isomorphic-unfetch");
 const { promises: fs } = require("fs");
 const path = require("path");
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // Disable certificate validation temporarily
-
 async function main() {
   const readmeTemplate = (
     await fs.readFile(path.join(process.cwd(), "./README.template.md"))
   ).toString("utf-8");
 
-  const quote = await (
-    await fetch("https://api.quotable.io/quotes/random")
-  ).json();
+  // const quote = await (
+  //   await fetch("https://api.quotable.io/quotes/random")
+  // ).json();
 
-  console.log(quote);
+  // console.log(quote);
 
   const readme = readmeTemplate
-    .replace("{quote}", quote.content)
-    .replace("{quote_author}", `- ${quote.author}`);
+    // .replace("{quote}", quote.content)
+    // .replace("{quote_author}", `- ${quote.author}`);
 
   await fs.writeFile("README.md", readme);
 }
